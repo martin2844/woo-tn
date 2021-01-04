35fda96a9d6cf8c4ca561b095bdedcab0b67ca31
Martin Chammah <martin.chammah@ecomerciar.com>
2021-01-04 20:40:24 -0300

    yml and port changes

--- app.js
+++ app.js
@@ -92,11 +92,11 @@ process.on('unhandledException', (reason, p) => {
     throw reason;
   });
   
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6750;
 // 
 
 // if(process.env.NODE_ENV === "test") {
-if(false){
+if(true){
     app.listen(PORT, console.log(`server started on ${PORT}`));
 } else {
     // For https on localhost
